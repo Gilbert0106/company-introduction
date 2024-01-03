7d09e2542877e88dfb5e5218e0b8a774860204f7
Giblert Brandow <simon@0100.se>
2024-01-03 22:23:48 +0100

    Updated main.py to save file as pdf using reportlab lib

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import sys
+from datetime import date
+from reportlab.pdfgen.canvas import Canvas
+
 
 def main():
     if not len(sys.argv) == 2:
-        sys.exit("Usage: python main.py TICKER")
-    
+        sys.exit("Usage: python main.py [TICKER SYMBOL]")
+
     ticker = sys.argv[1]
-    if len(ticker) < 2 or len(ticker) > 5: 
+    
+    if len(ticker) < 2 or len(ticker) > 5:
         sys.exit("A valid ticker must be between 2 and 5 characters long.")
 
-# Run Main function
+    # Get date
+    today = date.today()
+
+    # Start painting canvas
+    canvas = Canvas("reports/" + ticker + "-" + today.strftime("%d%m%y") + ".pdf")
+    canvas.drawString(72, 72, "Hello, World!")
+
+    # Save pdf
+    canvas.save()
+
+
+# Call main()
 if __name__ == "__main__":
     main()
