d8e9c5f8c81433850833d32ed6ea88be9fc1add1
Giblert Brandow <simon@0100.se>
2024-01-03 15:34:34 +0100

    Updated main.py to accept command line arguments fo ticker instead of asking for input

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    ticker = input("Ticker symbol: ")
-    print(ticker)
+import sys
 
-if __name__ == "__main__": 
+def main():
+    if not len(sys.argv) == 2:
+        sys.exit("Usage: python main.py TICKER")
+    
+    ticker = sys.argv[1]
+    if len(ticker) < 2 or len(ticker) > 5: 
+        sys.exit("A valid ticker must be between 2 and 5 characters long.")
+
+# Run Main function
+if __name__ == "__main__":
     main()
