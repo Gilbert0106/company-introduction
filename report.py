f390035194f50bb561a8b08f77c4096929edecbb
Giblert Brandow <simon@0100.se>
2024-01-07 20:06:28 +0100

    Refactored and minor adjustments to title and summary

--- report.py
+++ report.py
@@ -3,14 +3,14 @@ from reportlab.pdfgen.canvas import Canvas
 from reportlab.lib.pagesizes import A4
 import os.path
 
-
 class Report(object):
 
     w, h = A4
 
     def __init__(self, ticker):
         self.ticker = ticker
-        self.filepath = "reports/" + ticker + "-" + date.today().strftime("%d%m%y") + ".pdf"
+        self.filepath = "reports/" + ticker + "-" + \
+            date.today().strftime("%d%m%y") + ".pdf"
 
         if os.path.isfile(self.filepath):
             raise Exception(
@@ -21,24 +21,25 @@ class Report(object):
 
     def add_title(self, name):
         self.canvas.setFont(psfontname='Times-Roman', size=24)
-        self.canvas.drawString(32, self.h - 50, f'Introduction of:  {name}')
+        self.canvas.drawString(32, self.h - 50, f'Introduction of:  {name} ({self.ticker})')
 
     def add_business_summary(self, summary):
-        self.canvas.drawString(72, self.h - 50, summary)
+        self.canvas.setFont(psfontname='Times-Roman', size=12)
+        self.canvas.drawString(32, self.h - 82, summary)
 
     def add_footer(self):
         self.canvas.setFont(psfontname='Times-Roman', size=12)
         self.canvas.drawString(self.w - 72, 32, f'Page {self.pages_count}.')
 
     def new_page(self):
-        if not hasattr(self, 'canvas'): 
+        if not hasattr(self, 'canvas'):
             # Create first page
             self.pages_count = 1
             self.canvas = Canvas(self.filepath, pagesize=A4)
-        else: 
+        else:
             self.canvas.showPage()
             self.pages_count += 1
-        
+
         self.add_footer()
 
     def save(self):
