993db650c445738c20f6eeed114a62f1b1e3ea86
Giblert Brandow <simon@0100.se>
2024-01-10 22:28:27 +0100

    Expanded CompanyApi object with methods for retrieving simple data

--- source/companyApi.py
+++ source/companyApi.py
@@ -2,9 +2,19 @@ import yfinance as yf
 
 
 class CompanyApi(object):
-      def __init__(self, ticker):
+    def __init__(self, ticker: str) -> None:
         # Check if ticker is a valid symbol by fetching data from API
         try:
-            self.info = yf.Ticker(ticker).info
+            self.handle = yf.Ticker(ticker)
+            self.info = self.handle.info
         except:
             raise Exception(ticker + " does not seem to be a valid ticker.")
+
+    def getName(self) -> str:
+        return self.info['shortName']
+
+    def getSymbol(self) -> str:
+        return self.info['symbol']
+
+    def getSummary(self) -> str:
+        return self.info['longBusinesssSummary']
